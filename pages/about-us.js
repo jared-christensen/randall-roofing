b0f1f28491255f0b620d32a0ec6f930fe89c63e6
Jared Christensen <jared.christensen@gmail.com>
2020-07-15 17:07:48 -0500

    moved to static html

--- pages/about-us.js
+++
@@ -1,8 +0,0 @@
-import Container from "../components/container";
-export default function Services() {
-  return (
-    <>
-      <Container pageTitle="Services">About Us</Container>
-    </>
-  );
-}
